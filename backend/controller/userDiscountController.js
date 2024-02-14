ef97f9bdc57eeb762788bdfb50887c38f30430b6
asitouze <aurelien.sitouze@gmail.com>
2024-02-14 16:17:57 +0100

    save for date

--- backend/controller/userDiscountController.js
+++ backend/controller/userDiscountController.js
@@ -1,3 +1,4 @@
+const discountModel = require("../models/discountModel");
 const userDiscountModel = require("../models/userDiscountModel");
 
 const userDiscountController = {
@@ -13,17 +14,59 @@ const userDiscountController = {
   addUserDiscount: async (req, res) => {
     try {
       const userDiscount = req.body;
-      const result = await userDiscountModel.queryAddUserDiscount(userDiscount);
-      const [updateResult] = await userDiscountModel.decrementdiscountQuantity(
-        userDiscount.discount_id
-      );
-      // console.log('stockEvent', stockEvent);
+
+      const [date] = await discountModel.queryGetAllDiscount();
+      // console.log("duree_de_validite", test[0].duree_de_validite);
+
+      // console.log("Date.now()", Date.now());
+
+      // Obtenir la date actuelle
+      let dateActuelle = new Date();
+      // let dateFormatee = dateActuelle.toISOString();
+
+      // console.log(dateFormatee);
+
+      // const [updateResult] = await userDiscountModel.decrementdiscountQuantity(
+      //   userDiscount.discount_id
+      // );
       // console.log('result', result);
       // console.log('updateResult', updateResult);
-      if (updateResult.affectedRows) {
-        res.json({ message: "Code promos utilisé" });
+      // date.map((dates) => {
+      //   dates.duree_de_validite;
+      //   // console.log("dates.duree_de_validite");
+      //   let dateValidite = new Date(dates.duree_de_validite);
+      //   console.log("dateValidite", dateValidite);
+      //   console.log("dateFormatee", dateFormatee);
+      // });
+      let dateValidite = new Date(
+        date[userDiscount.discount_id - 1].duree_de_validite
+      );
+      // console.log("dateValidite", dateValidite);
+      // console.log("dateActuelle", dateActuelle);
+      // console.log(
+      //   "dateActuelle <= dateValidite",
+      //   dateActuelle.getTime() <= dateValidite.getTime()
+      // );
+      // console.log(
+      //   "dateActuelle > dateValidite",
+      //   dateActuelle.getTime() > dateValidite.getTime()
+      // );
+
+      if (dateActuelle.getTime() <= dateValidite.getTime()) {
+        const result = await userDiscountModel.queryAddUserDiscount(
+          userDiscount
+        );
+        const [updateResult] =
+          await userDiscountModel.decrementdiscountQuantity(
+            userDiscount.discount_id
+          );
+        if (updateResult.affectedRows) {
+          res.json({ message: "Code promos utilisé" });
+        } else {
+          res.json({ message: "Code promos invalide" });
+        }
       } else {
-        res.json({ message: "Error" });
+        res.json({ message: "Code promo expiré" });
       }
     } catch (error) {
       res.status(500).json({ error: error.toString() });
